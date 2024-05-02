50a4e4ed153b9e5d2acc241f16fa26c6a4d9c04a
T_Mori <morit0301@gmail.com>
2024-05-03 04:51:13 +0900

    ✨ API（register, login, JWT token）

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+
 const app = express();
 const PORT = 8000;
 require("dotenv").config();
+
+app.use(express.json());
+app.use("/api/v1", require("./src/v1/routes/auth"));
 
 // app.get("/", (req, res) => {
 //   res.send("Hello Express");
@@ -15,10 +19,6 @@ try {
 } catch (error) {
   console.log(error);
 }
-
-// ユーザー新規登録API
-
-// ユーザーログイン用API
 
 app.listen(PORT, () => {
   console.log("ローカルサーバー起動中・・・");
